c94bdcefd2502152921cab068f6eb1a48530a043
Jing Liu <jing.liu@rea-group.com>
2019-05-11 19:05:40 +1000

    change photo rotation

--- src/NewsContainer.js
+++ src/NewsContainer.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import NewsList from './news-list'
 import resultData from './data.json';
+import photo1 from './images/motherWechatPhoto.jpeg'
+import photo2 from './images/momAndMeInPlantsGarden.jpeg'
+import photo3 from './images/Sister.jpeg'
+import photo4 from '././images/threeOfUs.jpg'
 
+const images = [photo1, photo2, photo3,photo4]
 class NewsContainer extends React.Component {
     constructor(porps) {
         super(porps);
@@ -9,7 +14,12 @@ class NewsContainer extends React.Component {
     }
 
     componentDidMount() {
-        const result = resultData;
+        const result = resultData.map((item,index)=>{
+            return {
+                ...item,
+                imgSrc: images[index]
+            }
+        });
 
         this.setState({ data: result })
     }
