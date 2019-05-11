b3401ad04511e29d30c312f85fe99747848104ad
Jing Liu <jing.liu@rea-group.com>
2019-05-11 13:38:59 +1000

    make the site to be responsive

--- src/news-card/Image.js
+++ src/news-card/Image.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Image(props) {
     return (
         <div className="newsImage">
-            <img src={props.src} alt='property'></img>
+            <img src={props.src} alt='property' sizes="(max-width: 300px) 100vw, 300px"></img>
         </div>
     );
 }
